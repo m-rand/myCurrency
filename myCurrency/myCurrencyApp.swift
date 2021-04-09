a2ab0650c9c3a39726514dcf8c0d6a114b8af3e0
m-rand <marcel.balas@icloud.com>
2021-04-09 20:35:23 +0200

    Initial Commit

---
+++ myCurrency/myCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCurrencyApp.swift
+//  myCurrency
+//
+//  Created by Marcel Baláš on 09.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct myCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
